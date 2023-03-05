7ead4b90face0682ccc4571a110b3767dda0c794
lahirusamith <lahirusmp@gmail.com>
2023-03-05 21:07:19 +0800

    add todo page component

---
+++ src/Todo/Todo.jsx
@@ -0,0 +1,11 @@
+import { Row } from 'antd';
+
+const Todo = () => {
+    return (<>
+        <Row className='page-margin'>
+            <p>Todo works</p>
+        </Row>
+    </>);
+};
+
+export default Todo;
